797eebedadf09de47a78a2d7096d3763d49fdb69
Ahmadjajja <ahmadjajja86@gmail.com>
2024-10-14 14:55:35 +0500

    section-a Class 5 Introduction

---
+++ Section_A/3. Python/1. Class 5 Introduction/even_odd.py
@@ -0,0 +1,11 @@
+
+
+# Loop through numbers 1 to 10
+start = 1
+for number in range(start, 11):
+    
+    # Check if the number is even or odd
+    if number % 2 == 0:
+        print(f"{number} is even")
+    else:
+        print(f"{number} is odd")
---
+++ Section_A/3. Python/1. Class 5 Introduction/hello.py
@@ -0,0 +1,9 @@
+
+# Purposes:
+
+# Brief overview of Python: High-level, interpreted, and general-purpose language.
+# Uses of Python: Web development, automation, data science, AI, and more.
+# Discuss Python's popularity due to its simplicity and large community support.
+
+
+print("hello world !!")
