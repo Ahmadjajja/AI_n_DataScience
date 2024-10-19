fd43320d2ef48b4307fa40e89ef5070d12d3d771
Ahmadjajja <ahmadjajja86@gmail.com>
2024-10-19 19:55:44 +0500

    SecA Class6

---
+++ Section_A/3. Python/2. Class 6/class2.py
@@ -0,0 +1,53 @@
+# Variables:
+            # Variable are just like container where we can store multiple
+            # type of data.
+            # Example - You can take glass as an example.
+
+
+# Data Types
+            # 1. Number (Integer & float) => 1, 1.0
+            # 2. String => "Ahmad"
+            # 3. Boolean => true & false
+            # 4. List
+            # 5. Objects (Dictionary)
+            
+# Assignment Operator
+            # It used for assigning value to the variable.
+            
+# num1 = 5
+# num2 = 10
+
+# sum = num1 + num2
+
+# # Display output
+#             # We use print statement to display the output
+            
+# print("sum : ", sum)
+# print("sum : " + str(sum))
+# print(f"sum : {sum}")
+
+
+
+# Data types: integers (4, 5), floats (12.3, 21.6), strings ("Ahmad"), booleans(true, false)
+
+
+# Type conversion (implicit & explicit)
+
+# There are 2 types of type conversions
+# 1. implicit
+
+# numsSum = 2 + 4.0
+# print(numsSum)
+# print(f"{numsSum}")
+
+# 2. explicit
+
+x = 5
+y = 4
+print(x + y) # 9
+print(x + float(y)) # 9.0
+print(float(x) + y) # 9.0
+print(float(x) + float(y)) # 9.0
+print(str(x) + str(y)) #54
+
+
