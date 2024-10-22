d16f584aee68f60e42e548782ac60f9da464285b
Ahmadjajja <ahmadjajja86@gmail.com>
2024-10-22 21:07:02 +0500

    class 5 & 6 sec-a

---
+++ Section_B/3. Python/1. Class 5 & 6/class1.py
@@ -0,0 +1,21 @@
+x = 6
+
+for i in range(1, x):
+    print("asdfdf")
+    
+    if i % 2 == 0:
+        
+        print("It is even number:", i)
+        
+    else:
+        
+        print("It is odd number:", i)
+        
+
+
+
+
+
+
+
+    
---
+++ Section_B/3. Python/1. Class 5 & 6/class2.py
@@ -0,0 +1,14 @@
+# x = 5
+# y = 10
+# temp = y
+# y = x
+# x = temp
+# # x, y = y, x
+# print("value of x is", x,"value of y is",y)
+
+first_Name = "Danish"
+second_Name = "Mustafa"
+Value = 20
+
+Name = first_Name + " " + second_Name + str(Value)
+print(Name)
