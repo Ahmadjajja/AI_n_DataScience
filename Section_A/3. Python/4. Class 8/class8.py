cbd396504f61afce0866c03c8e5c7c4958819410
Ahmadjajja <ahmadjajja86@gmail.com>
2024-10-22 21:05:26 +0500

    class 8

---
+++ Section_A/3. Python/4. Class 8/class8.py
@@ -0,0 +1,107 @@
+# # # 4. Control Structures
+# #                     # Conditional statements: if, else, elif
+# # # cricket_score = 22
+
+# # # if cricket_score >= 200:
+# # #     print("Babar Azam hit a double century.")
+# # # elif cricket_score >= 100:
+# # #     print("Babar Azam hit a century.")
+# # # elif cricket_score >= 50:
+# # #     print("Babar Azam hit a fifty.")
+# # # elif cricket_score >= 30:
+# # #     print("Babar Azam hit a thirty.")
+# # # else:
+# # #     print("Babar Azam scored " + str(cricket_score) + " runs.")
+
+    
+# #                     # Logical operators: and, or, not
+# # print("Logical operators: and, or, not")
+
+# # # and operator:
+# # print("____________")
+# # print("and operator")
+# # print("____________")
+# # print("True and True : " + str(True and True))
+# # print("False and True : " + str(False and True))
+# # print("True and False : " + str(True and False))
+# # print("False and False : " + str(False and False))
+
+# # print("____________")
+# # print("or operator")
+# # print("____________")
+# # print("True or True : " + str(True or True))
+# # print("False or True : " + str(False or True))
+# # print("True or False : " + str(True or False))
+# # print("False or False : " + str(False or False))
+
+# # print("____________")
+# # print("not operator")
+# # print("____________")
+# # print("not True : " + str(not True))
+# # print("not False : " + str(not False))
+#                     # Comparison operators
+
+# # There are multiple comparison programming:
+# #     1. ==, >, <, >=, <=,!=
+
+#                     # Loops: for, while
+# # student1 = "talha"
+# # student2 = "ahmad"
+# # student3 = "amaar"
+# # .
+# # .
+# # .
+# # studentn = "ramzan"
+
+# students = ["talha", "ahmad", "amar", "azeem","ali"]
+
+# # print(students[0])
+# # print(students[1])
+# # print(students[2])
+# # print(students[3])
+# # print(students[4])
+# # print(len(students))
+# # print(students[ len(students) - 1 ])
+#                     # Loop control: break, continue, pass
+                    
+# # for i in range(0, 5):
+# #     print(0)
+# #     if i == 0:
+# #         break
+# #     print(1)
+# #     if i == 1:
+# #         break
+# #     print(2)
+# #     if i == 2:
+# #         break
+# #     print(students[i])
+# #     print("i ", i)
+# #     print(3)
+# #     if i == 3:
+# #         break
+# #     print(4)
+# #     if i == 4:
+# #         break
+                    
+# for i in range(0, 5):
+#     print(0)
+#     if i == 0:
+#         continue
+#     print(1)
+#     if i == 1:
+#         continue
+#     print(2)
+#     if i == 2:
+#         continue
+#     print(students[i])
+#     print("i ", i)
+#     print(3)
+#     if i == 3:
+#         continue
+#     print(4)
+#     if i == 4:
+#         continue
+    
+    
+for i in range(0, 4):
+    pass # to-do nothing
