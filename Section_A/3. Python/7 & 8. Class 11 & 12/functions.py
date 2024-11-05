46170b6e257f95c89affd662ed79924c16e978d2
Ahmadjajja <ahmadjajja86@gmail.com>
2024-11-05 20:14:41 +0500

    7 & 8. Class 11 & 12

---
+++ Section_A/3. Python/7 & 8. Class 11 & 12/functions.py
@@ -0,0 +1,72 @@
+# 5. Functions
+#   Functions are just a block of code that can be used perform specific task.
+#   Benefits: (i) reuseability of the code 
+
+# # write a function that should print 1 to 10
+
+# def printCounting():   # this is how we define the function
+#     for i in range(1, 11):
+#         print(i)
+    
+# print("___________")
+# printCounting() # this is how we call the function
+# print("___________")
+# printCounting() # this is how we call the function
+
+
+# Greet the user
+y = "20"
+def greet():
+    x = "10"
+    print("Good afternoon everyone !!!")
+    print("How are you doing?")
+    print("Please sit down.")
+
+greet()
+print(".........")
+print('asdf')
+print('asdflkjh')
+print(".........")
+greet()
+
+
+
+
+
+
+
+
+# There are 2 types of functions
+# 1. user-defined
+    # example
+    
+def greet(): # defination of function
+    print("hello")
+    
+greet() # function calling
+    
+# 2. built-in functions
+    # example
+    
+print("Ahmad") # function calling
+
+
+
+
+
+
+
+
+
+
+
+
+
+# why we defined this function? what's the purpose?
+
+
+
+# Defining and calling functions
+# Parameters, arguments and return values
+# Default arguments
+# Function scope: local vs global
