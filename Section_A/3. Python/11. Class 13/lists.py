114ae23428600c28b0a6fa8b2124e031291d2dec
Ahmadjajja <ahmadjajja86@gmail.com>
2024-11-11 16:51:03 +0500

    11. Class 13

---
+++ Section_A/3. Python/11. Class 13/lists.py
@@ -0,0 +1,105 @@
+
+
+# lists
+    # U can say it like combination of multiple variables
+    # U can store list of values in it of multiple types
+
+# studentsList = ["Abdullah", "Shaharyar", "Faizan", "Abdul Aziz"]
+
+# # Camel Case: first character of first word is small and first character of later words is capital
+# # Pascal Case: first character of first word is Capital and first character of later words is capital
+
+# # student1 = "Abdullah"
+# # student2 = "Shaharyar"
+# # student3 = "Faizan"
+# # student4 = "Abdul Aziz"
+
+# print(studentsList[0]) # Abdullah
+# print(studentsList[3]) # Abdul Aziz
+
+
+# student1Data = ["Ali", 22, 22, 22, 22, 22, 22, "SMIT Student", 22, "31102-5637235-4"]
+# print(student1Data)
+# student1Data.append("last value")
+# print(student1Data)
+# student1Data.insert(3, "FSD")
+# print(student1Data)
+# student1Data.remove("last value")
+# print(student1Data)
+# student1Data.pop()
+# print(student1Data)
+# student1Data.pop(2)
+# print(student1Data)
+# print(student1Data[::-1])
+# print(student1Data.index(22))
+# print(student1Data.count(22))
+# student1Data.clear()
+# student1Data.reverse()
+# print(student1Data)
+
+
+nums = [4, 2, 5, 2, 2, 1, 3, 10]
+print("nums : ", nums)
+nums.sort(reverse=True)
+print("nums in des order : ", nums)
+
+
+
+
+# print(student1Data)
+
+# # Methods
+# list()
+
+# print(list("Ahmad"))
+
+
+
+# list[start:end:step]
+
+# print(student1Data[1: 4: 2])
+
+
+
+
+# fruits.append("orange")
+# fruits.insert(1, "mango")  # Insert "mango" at index 1
+# fruits.remove("banana")
+# fruits.pop()     # Removes last item
+# fruits.pop(0)
+
+
+
+
+# fruits.index("cherry")
+# numbers.count(3)
+
+# numbers.sort()            # Ascending
+# numbers.sort(reverse=True)
+
+# fruits.reverse()
+# fruits_copy = fruits.copy()
+# fruits.clear()
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
