a3013dba57436d02d953b55e1b18468e8d1b9125
Ahmadjajja <ahmadjajja86@gmail.com>
2024-11-12 14:55:25 +0500

    12. Class 14

---
+++ Section_A/3. Python/12. Class 14/Quiz.py
@@ -0,0 +1,41 @@
+# The language you're learning here
+# 1: print
+# 2: Variables for Strings
+# 3: Variables for Numbers
+# 4: Math expressions: Familiar operators
+# 5: Variable Names Legal and Illegal
+# 6: Math expressions: Unfamiliar operators
+# 7: Math expressions: Eliminating ambiguity
+# 8: Concatenating text strings
+# 9: if statements
+# 10: Comparison operators
+# 11: else and elif statements
+# 12: Testing sets of conditions
+# 13: if statements nested
+# 14: Comments
+# 15: Lists
+# 16: Lists: Adding and changing elements
+# 17: Lists: Taking slices out of them
+# 18: Lists: Deleting and removing elements
+# 19: Lists: popping elements
+# 20: skip at the moment
+# 21: for loops
+# 22: for loops nested
+
+# GuyWhoKeepsSayingHisOwnName
+
+
+num1 = 450
+
+if num1 < 100:
+    print("num is less than 100")
+elif num1 <= 500:
+    print("Num is less than 500")
+else:
+    print("Num is greater than 500")
+    
+    
+str = "[1, 4, 5, 3]"
+
+for ch in str:
+    print("ch : ", ch)
