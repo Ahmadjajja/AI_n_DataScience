e3790faddb2cd25b45575f46fecc5291a3b3f03c
Ahmadjajja <ahmadjajja86@gmail.com>
2024-11-18 14:54:04 +0500

    5. Class 11

---
+++ Section_B/3. Python/5. Class 11/revision.py
@@ -0,0 +1,7 @@
+userNames = ["Ahmad","ystad", "badd", "zli", "hrham"]
+Name = "Ahmad" # search ahmad
+
+for i in range(0, 100000000):
+    # if Name == userNames[i]:
+    #     # add name in the list to show to user
+    print(i)
