27a5af693ff47202e0984f663bb8870292a87ca5
Ahmadjajja <ahmadjajja86@gmail.com>
2024-11-26 23:51:13 +0500

    16. Class 18

---
+++ Section_A/3. Python/16. Class 18/class18.py
@@ -0,0 +1,14 @@
+name = '''
+Set the target for next week
+1. Complete functions assignment
+2. Read and Practice the first 52 chapters
+   of book "smarter way to learn python.
+'''
+
+print(name)
+# print("name -> ", name.lower())
+# print("name -> ", name.upper())
+# student = {
+#     name: "Ahmad",
+#     languages: ["URDU", "PUNJABI", "ENGLISH"]
+# }
