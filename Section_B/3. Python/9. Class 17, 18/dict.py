2585468b6e7bc7f962fb481414e652e0399f1706
Ahmadjajja <ahmadjajja86@gmail.com>
2024-11-30 21:32:28 +0500

    9. Class 17, 18

---
+++ Section_B/3. Python/9. Class 17, 18/dict.py
@@ -0,0 +1,59 @@
+# Dictionaries
+# It is an iterator data structure that stores values in the form key value pair.
+
+# remote = {
+#     "company": "Optoma",
+#     "color": "white",
+#     "total_buttons": "Eighteen"
+# }
+
+# print(remote)
+# remote["button_color"] = "gray"
+# print(remote)
+
+# print(remote["company"])
+# remote["company"] = "Intel"
+# print(remote["company"])
+# print(remote["total_buttons"])
+
+# for key, value in remote.items():
+#     print(key, " : ", value)
+# print(remote)
+# print(remote.keys())
+# print(remote.values())
+
+
+# remote = {
+#     1: "Optoma",
+#     2: "white",
+#     3: "Eighteen"
+# }
+
+# print(remote[2])
+
+
+# Creating a list of dictionaries of ai & ds sec-b
+
+# section_b = [
+#     {"first_name": "Muhammad", "second_name" : "Abdullah", "roll_no": "1"},
+#     {"first_name": "Muhammad", "second_name" : "Saad", "roll_no": "2"},
+#     ]
+
+# print("before : ", section_b)
+# print("_________________________")
+# print("_________________________")
+# print("_________________________")
+# print("_________________________")
+
+# section_b.append({"first_name": "Ali", "second_name" : "Raza", "roll_no": "3"},)
+
+# print("after :", section_b)
+
+# Creating a dictionary that contains lists
+
+person1 = {
+    name: "Ali Hassan",
+    age: "20",
+    fav_games: ["PUBG", "GTA-5", "Call of duty", "Ludo", "Free-fire"],
+    fav_movies: ["shutter_island", "iron_man", "game_of_throne"]
+}
