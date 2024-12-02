3978ca39db4fa9a6f631cf538fd87cbd469657f2
Ahmadjajja <ahmadjajja86@gmail.com>
2024-12-02 15:04:25 +0500

    Updated

---
+++ Section_A/3. Python/15. Class 19/ass3.py
@@ -0,0 +1,43 @@
+# Create a dictionary student with keys: name, age, and grade. 
+    # Assign them appropriate values.
+
+student = {}
+
+# print(student)
+
+student["name"] = "hamza"
+student["grade"] = "A"
+
+print("age" in student)
+
+
+
+
+
+# # Access the value of the key grade in the student dictionary.
+# # print(student["grade"])
+
+# # Add a new key city to the student dictionary and set its value to "New York".
+# student["city"] = "FSD"
+
+# # Update the value of the age key in the student dictionary to 20.
+# student["age"] = 20
+
+# # Remove the key city from the student dictionary.
+# del student["city"]
+# print("student dict : ", student)
+
+# # Iterate through the dictionary student and print all keys.
+# for key in student.keys():
+#     print(key)
+    
+# # Iterate through the dictionary student and print all values.
+# for value in student.values():
+#     print(value)
+    
+# # Iterate through the dictionary student and print all key-value pairs in the format key: value.
+
+# for key, val in student.items():
+#     print(key, ":", val)
+#     # print(str(key) + " : " + str(val))
+    
