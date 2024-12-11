f67b605b3ed3f56091744b56a280d0d99ffb2668
Ahmadjajja <ahmadjajja86@gmail.com>
2024-12-11 21:13:31 +0500

    updated

---
+++ Section_A/3. Python/18. Class 22/hello.py
@@ -0,0 +1,18 @@
+# def contains_duplicate(nums):
+#     nums.sort()
+#     for i in range(len(nums) - 1):
+#         if nums[i] == nums[i + 1]:
+#             return True
+#     return False
+    
+# print(contains_duplicate([1,2,3,4]))
+
+def func(s, t):
+    s_hash = {}
+    t_hash = {}
+    
+    for ch in s:
+        
+
+
+print(func("anagram", "nagaram"))
